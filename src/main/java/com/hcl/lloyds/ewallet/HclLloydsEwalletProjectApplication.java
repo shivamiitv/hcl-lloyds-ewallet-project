ded0eedd0327b44b62449d824c49a2019e69ae18
shivamiitv <shivam.kumar.cse13.iitbhu@gmail.com>
2025-11-08 13:14:02 +0530

    initial project

---
+++ src/main/java/com/hcl/lloyds/ewallet/HclLloydsEwalletProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.lloyds.ewallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HclLloydsEwalletProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HclLloydsEwalletProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcl/lloyds/ewallet/HclLloydsEwalletProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.lloyds.ewallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HclLloydsEwalletProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
