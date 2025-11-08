4708515104d8e211f8ce68ebb2847eeaeb49361e
Jagan <busijagannathrao.080@gmail.com>
2025-11-08 13:11:28 +0530

    Initial commit

---
+++ src/main/java/com/ewallet/hcl_ewallet/HclEwalletApplication.java
@@ -0,0 +1,13 @@
+package com.ewallet.hcl_ewallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HclEwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HclEwalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ewallet/hcl_ewallet/HclEwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewallet.hcl_ewallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HclEwalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
