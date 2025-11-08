a48d12c98193cb6e514249c4fe9f47d1572dec2e
Jagan <busijagannathrao.080@gmail.com>
2025-11-08 17:21:35 +0530

    phone number, name, email validation implemented, and code refactor done.

--- src/main/java/com/hcl/lloyds/ewallet/dto/UserRequestDto.java
+++ src/main/java/com/hcl/lloyds/ewallet/dto/UserRequestDto.java
@@ -3,10 +3,15 @@ package com.hcl.lloyds.ewallet.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Data
+@Getter @Setter
 public class UserRequestDto {
     @NotBlank
-    @Size(max = 100)
+    @Size(max = 30)
     private String name;
 
     @Email
@@ -17,37 +22,4 @@ public class UserRequestDto {
     @Size(max = 15)
     private String phone;
 
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    @Override
-    public String toString() {
-        return "UserRequestDto{" +
-                "name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", phone='" + phone + '\'' +
-                '}';
-    }
 }
--- src/main/java/com/hcl/lloyds/ewallet/dto/UserResponseDto.java
+++ src/main/java/com/hcl/lloyds/ewallet/dto/UserResponseDto.java
@@ -1,5 +1,11 @@
 package com.hcl.lloyds.ewallet.dto;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+@Data
+@Setter
+@Getter
 public class UserResponseDto {
     private Integer userId;
     private String name;
@@ -11,47 +17,6 @@ public class UserResponseDto {
         this.name = name;
         this.email = email;
         this.phone = phone;
-    }
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    @Override
-    public String toString() {
-        return "UserResponseDto{" +
-                "userId=" + userId +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", phone='" + phone + '\'' +
-                '}';
     }
 
 }
