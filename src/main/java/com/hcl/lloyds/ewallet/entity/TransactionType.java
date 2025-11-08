b3c9646961e42159c19db178bd3edc8834942c56
Jagan <busijagannathrao.080@gmail.com>
2025-11-08 15:08:38 +0530

    added swagger, mysql dependency and application.property settings.

--- src/main/java/com/hcl/lloyds/ewallet/entity/TransactionType.java
+++
@@ -1,5 +0,0 @@
-package com.hcl.lloyds.ewallet.entity;
-
-public enum TransactionType {
-    CREDIT, DEBIT, TRANSFER_IN, TRANSFER_OUT
-}
--- src/main/java/com/hcl/lloyds/ewallet/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hcl.lloyds.ewallet.repository;
-
-import com.hcl.lloyds.ewallet.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByPhone(String phone);
-}
